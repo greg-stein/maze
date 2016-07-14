077c1cb3002dc8857c3070f9e9fbde7c50be463c
Greg Stein <stein.gregory@gmail.com>
2016-07-14 10:17:52 +0300

    Added calculations for wall coordinates depending on two given points

--- app/src/main/java/com/example/neutrino/maze/Wall.java
+++ app/src/main/java/com/example/neutrino/maze/Wall.java
@@ -1,9 +1,7 @@
 package com.example.neutrino.maze;
 
-import android.opengl.GLES20;
+import android.graphics.Point;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
 
@@ -13,7 +11,7 @@ import java.nio.ShortBuffer;
 public class Wall {
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
-    static float wallCoords[] = {
+    static float mCoords[] = {
             // in counterclockwise order:
             -0.5f,  0.6f, 0.0f,   // top left
             -0.5f,  0.5f, 0.0f,   // bottom left
@@ -21,20 +19,54 @@ public class Wall {
             0.5f,   0.6f, 0.0f,   // top right
     };
 
-    private final short drawOrder[] = { 0, 1, 2, 0, 2, 3 }; // order to draw vertices
+    private final short mDrawOrder[] = { 0, 1, 2,   // first triangle
+                                         1, 2, 3 }; // second triangle
+
     private int mVertexBufferPosition;
     private int mIndexBufferPosition;
+    
+    private float mA[] = {-0.5f,  0.5f};
+    private float mB[] = {0.5f,   -0.5f};
+    private float mWidth = 0.05f;
 
     public Wall() {
+        calcCoords();
+    }
+
+    private void calcCoords() {
+        float[] vector = {mA[0]-mB[0], mA[1]-mB[1]};
+        float magnitude = (float) Math.sqrt(vector[0]*vector[0] + vector[1]*vector[1]);
+        float[] identityVector = {vector[0]/magnitude, vector[1]/magnitude};
+        float[] orthogonalIdentityVector = {identityVector[1], -identityVector[0]};
+
+        mCoords[0] = mA[0] + mWidth * orthogonalIdentityVector[0];
+        mCoords[1] = mA[1] + mWidth * orthogonalIdentityVector[1];
+
+        mCoords[3] = mA[0] - mWidth * orthogonalIdentityVector[0];
+        mCoords[4] = mA[1] - mWidth * orthogonalIdentityVector[1];
+
+        mCoords[6] = mB[0] + mWidth * orthogonalIdentityVector[0];
+        mCoords[7] = mB[1] + mWidth * orthogonalIdentityVector[1];
+
+        mCoords[9] = mB[0] - mWidth * orthogonalIdentityVector[0];
+        mCoords[10] = mB[1] - mWidth * orthogonalIdentityVector[1];
     }
 
     public void putCoords(FloatBuffer vertexBuffer) {
         mVertexBufferPosition = vertexBuffer.position();
-        vertexBuffer.put(wallCoords);
+        vertexBuffer.put(mCoords);
     }
 
     public void putIndices(ShortBuffer indexBuffer) {
         mIndexBufferPosition = indexBuffer.position();
-        indexBuffer.put(drawOrder);
+        indexBuffer.put(mDrawOrder);
+    }
+
+    public float getWidth() {
+        return mWidth;
+    }
+
+    public void setWidth(float mWidth) {
+        this.mWidth = mWidth;
     }
 }
