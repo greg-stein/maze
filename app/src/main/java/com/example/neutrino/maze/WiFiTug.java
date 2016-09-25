1223527d573c8935d362de0a26885bffc9c04fd5
Greg Stein <stein.gregory@gmail.com>
2016-09-25 10:36:40 +0300

    Added TagOfWar concept + WiFi stub

---
+++ app/src/main/java/com/example/neutrino/maze/WiFiTug.java
@@ -0,0 +1,19 @@
+package com.example.neutrino.maze;
+
+import android.graphics.PointF;
+
+/**
+ * Created by Greg Stein on 9/25/2016.
+ */
+public class WiFiTug implements TugOfWar.ITugger {
+    @Override
+    public void getPosition(PointF position) {
+        // STUB: TODO
+    }
+
+    @Override
+    public float getForce() {
+        // STUB: TODO
+        return 0;
+    }
+}
