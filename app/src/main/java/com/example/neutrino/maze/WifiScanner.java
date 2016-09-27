89fad7dc4de101c4a81eb0e62ea451b7bed34193
Greg Stein <stein.gregory@gmail.com>
2016-09-27 14:13:44 +0300

    Added IFingerprintAvailableListener

--- app/src/main/java/com/example/neutrino/maze/WifiScanner.java
+++ app/src/main/java/com/example/neutrino/maze/WifiScanner.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Greg Stein on 8/10/2016.
  */
 public class WifiScanner extends BroadcastReceiver {
+    private static final int MAX_WIFI_LEVEL = 100; // Percent of signal reception
+
     private List<ScanResult> mLastScan;
     private WifiManager mWifiManager;
     private boolean mIsEnabled = false;
@@ -34,9 +38,30 @@ public class WifiScanner extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction() == WifiManager.SCAN_RESULTS_AVAILABLE_ACTION) {
             mLastScan = mWifiManager.getScanResults();
-
             if (mIsEnabled) mWifiManager.startScan();
+
+            emitWiFiFingerprintAvailableEvent(mLastScan);
             mScanId++;
+        }
+    }
+
+    public interface IFingerprintAvailableListener {
+        void onFingerprintAvailable(Map<String, Integer> fingerprint);
+    }
+    private IFingerprintAvailableListener mFingerprintAvailableListener = null;
+    public void setFingerprintAvailableListener(IFingerprintAvailableListener listener) {
+        this.mFingerprintAvailableListener = listener;
+    }
+    private void emitWiFiFingerprintAvailableEvent(List<ScanResult> scanResults) {
+        if (mFingerprintAvailableListener != null) {
+            Map<String, Integer> fingerprint = new HashMap<>();
+
+            for (ScanResult scan : scanResults) {
+                int level = WifiManager.calculateSignalLevel(scan.level, MAX_WIFI_LEVEL);
+                fingerprint.put(scan.BSSID, level);
+            }
+
+            mFingerprintAvailableListener.onFingerprintAvailable(fingerprint);
         }
     }
 }
