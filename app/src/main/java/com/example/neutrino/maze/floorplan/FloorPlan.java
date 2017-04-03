d6d989d5f41cff176d497516fa8e2f857d7bc7bb
Greg Stein <stein.gregory@gmail.com>
2017-04-03 14:45:19 +0300

    Added FloorPlan entity, Tag class

---
+++ app/src/main/java/com/example/neutrino/maze/floorplan/FloorPlan.java
@@ -0,0 +1,15 @@
+package com.example.neutrino.maze.floorplan;
+
+import java.util.List;
+
+/**
+ * Created by Greg Stein on 4/3/2017.
+ */
+
+public class FloorPlan {
+    private List<IFloorPlanPrimitive> mSketch;
+    private List<WifiMark> mFingerprints;
+    private List<Tag> mTags;
+    private List<String> mBuildingNames;
+    private String mFloorName;
+}
---
+++ app/src/main/java/com/example/neutrino/maze/floorplan/Tag.java
@@ -0,0 +1,27 @@
+package com.example.neutrino.maze.floorplan;
+
+/**
+ * Created by Greg Stein on 4/3/2017.
+ */
+public class Tag extends FloorPlanPrimitiveBase {
+
+    @Override
+    protected int getVerticesNum() {
+        return 0;
+    }
+
+    @Override
+    protected int getIndicesNum() {
+        return 0;
+    }
+
+    @Override
+    public void updateVertices() {
+
+    }
+
+    @Override
+    public void scaleVertices(float scaleFactor) {
+
+    }
+}
