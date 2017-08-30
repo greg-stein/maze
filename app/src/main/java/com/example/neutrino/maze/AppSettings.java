5fe23a50578ca13243ae849caa4c046586dfaff5
Greg Stein <stein.gregory@gmail.com>
2017-08-30 20:06:23 +0300

    Added step length calibrator fields + save/load to shared prefs

--- app/src/main/java/com/example/neutrino/maze/AppSettings.java
+++ app/src/main/java/com/example/neutrino/maze/AppSettings.java
@@ -2,12 +2,16 @@ package com.example.neutrino.maze;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 
 /**
  * Created by Greg Stein on 8/21/2016.
  */
 public class AppSettings {
+    public static final String CONFIG_FILE = "maze.conf";
+
     public static int locationMarkColor;
     public static int footprintColor;
     public static int wallColor;
@@ -21,6 +25,10 @@ public class AppSettings {
     public static int oglTextRenderProgram;
     public static boolean inDebug;
     public static int pathColor;
+    public static float calibratorWalkedDistance;
+    public static int calibratorStepsDetected;
+    public static boolean calibrationCompleted;
+    public static float calibratorUserStepLength;
 
     public static void init(Context context) {
         if (context instanceof Activity) {
@@ -36,5 +44,25 @@ public class AppSettings {
         editModeColor = ContextCompat.getColor(context, R.color.colorEditMode);
         accentColor = ContextCompat.getColor(context, R.color.colorAccent);
         inDebug = true;
+    }
+
+    public static void loadFromConfig(Context context) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        calibratorWalkedDistance = settings.getFloat("calibratorWalkedDistance", 0f);
+        calibratorStepsDetected = settings.getInt("calibratorStepsDetected", 0);
+        calibrationCompleted = settings.getBoolean("calibrationCompleted", false);
+        calibratorUserStepLength = settings.getFloat("calibratorUserStepLength", 0f);
+    }
+
+    public static void saveToConfig(Context context) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = settings.edit();
+
+        editor.
+                putFloat("calibratorWalkedDistance", calibratorWalkedDistance).
+                putInt("calibratorStepsDetected", calibratorStepsDetected).
+                putBoolean("calibrationCompleted", calibrationCompleted).
+                putFloat("calibratorUserStepLength", calibratorUserStepLength).
+                apply();
     }
 }
