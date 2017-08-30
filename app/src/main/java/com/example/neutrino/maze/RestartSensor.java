0c21717e89d6538e020a2866f0eea112f60db70a
Greg Stein <stein.gregory@gmail.com>
2017-08-30 20:05:05 +0300

    Kick-off forever running calibrator service

---
+++ app/src/main/java/com/example/neutrino/maze/RestartSensor.java
@@ -0,0 +1,16 @@
+package com.example.neutrino.maze;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Greg Stein on 8/30/2017.
+ */
+
+public class RestartSensor extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
