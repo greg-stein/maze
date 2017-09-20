09b1f41e6e1364990d3070e0d0a2ae7b4a10b977
Dima Ruinskiy <dima.ruinskiy@gmail.com>
2017-09-21 00:05:34 +0300

    Lift detector algorithm fixes: 1. Negative index bug in sliding window: Java's % operator is remainder, not modulo - so result can be negative    Replace X % WINDOW_SIZE with (X+WINDOW_SIZE) % WINDOW_SIZE - to ensure it's always positive    (in our code X is always smaller than WINDOW_SIZE) 2. Lowpass filter calculation fix 3. Median filter calculation fix (must compute it with (L-1)/2-sample delay) 4. Add debug output support conditional on flag

--- app/src/main/java/com/example/neutrino/maze/LiftDetector.java
+++ app/src/main/java/com/example/neutrino/maze/LiftDetector.java
@@ -13,7 +13,7 @@ public class LiftDetector implements SensorListener.IGravityChangedListener {
     enum LiftState {
         NOT_IN_MOVING_LIFT,
         IN_LIFT_GOING_UP,
-        IN_LIFT_GOING_DOWN,
+        IN_LIFT_GOING_DOWN
     };
 
     private static LiftDetector instance = new LiftDetector();
@@ -46,9 +46,13 @@ public class LiftDetector implements SensorListener.IGravityChangedListener {
     private boolean stateChanged;                   // Flag to indicate recent state changed
 
     private int k;              // Sliding window index
+    private int m;              // Median filter center index (lags behind window index)
+    private int n;              // Global event counter (for debug purposes)
     private float runningSum;   // Running sum for lowpass filter
 
     private List<IElevatorListener> mElevatorListeners = new ArrayList<>();
+
+    private boolean verboseSystemOutput;    // Whether to print detailed data to System.out
 
     private LiftDetector() {
         signalData = new float[WINDOW_SIZE];
@@ -58,24 +62,43 @@ public class LiftDetector implements SensorListener.IGravityChangedListener {
         medFiltArray = new float[MEDFILT_LENGTH];
 
         k = 0;
+        m = (WINDOW_SIZE - MEDFILT_LENGTH / 2) % WINDOW_SIZE;   // MEDFILT_LENGTH/2 samples delay
+        n = 0;
         runningSum = 0;
 
         currentState = LiftState.NOT_IN_MOVING_LIFT;
         thisSign = 0;
         currentNegLength = currentPosLength = lastNegLength = lastPosLength = 0;
+
+        verboseSystemOutput = false;
+    }
+
+    public void setVerboseSystemOutput(boolean v) {
+        verboseSystemOutput = v;
     }
 
     @Override
     public void onGravityChanged(float newGravity) {
-        signalData[k] = newGravity;                                             // Store new sample
-        runningSum -= signalData[(k - LOWPASS_LENGTH) % WINDOW_SIZE];           // Remove oldest sample
-        signalDataFiltered[k] = alpha * newGravity + (1 - alpha) * runningSum;  // Low-pass filtered sample
-        runningSum += newGravity;                                               // Add new sample to sum
-        signalDataSign[k] = Math.signum(signalDataFiltered[k]);                 // Sign of gravity
-        signalDataSignFiltered[k] = MedFiltSign();                              // Median-filtered sign function
+        signalData[k] = newGravity;                                                 // Store new sample
+        runningSum -= signalData[(k - LOWPASS_LENGTH + WINDOW_SIZE) % WINDOW_SIZE]; // Remove oldest sample
+        runningSum += newGravity;                                                   // Add new sample to sum
+        signalDataFiltered[k] = alpha * runningSum;                                 // Low-pass filtered sample
+        signalDataSign[k] = Math.signum(signalDataFiltered[k]);                     // Sign of gravity
+        signalDataSignFiltered[m] = MedFiltSign();                                  // Median-filtered sign function
 
-        thisSign = (int)signalDataSignFiltered[k];
+        if (verboseSystemOutput) {
+            System.out.printf("%4d Z=%+1.4f G=%+1.4f, S=%+d, F=%+d    ",
+                    n, signalData[k], signalDataFiltered[k], (int) signalDataSign[k], (int) signalDataSignFiltered[k]);
+            if (signalDataSignFiltered[m] != signalDataSignFiltered[(m - 1 + WINDOW_SIZE) % WINDOW_SIZE]) {
+                System.out.print(n - MEDFILT_LENGTH / 2);
+            }
+            System.out.print("\n");
+        }
+
+        thisSign = (int) signalDataSignFiltered[k];
         k = (k + 1) % WINDOW_SIZE;
+        m = (m + 1) % WINDOW_SIZE;
+        ++n;
 
         ++currentGap;
         // Once gap exceeds maximum allowed, last long streaks are considered irrelevant
@@ -142,11 +165,18 @@ public class LiftDetector implements SensorListener.IGravityChangedListener {
         }
     }
 
-    /* Return MEDFILT_LENGTH-point median filtering of the signal centered at the current index */
+    /* Return L-point median filtering of the signal centered at the current index (where L is the
+     * length of the median filter (L must be odd).
+     * Note that the median filter needs "future" samples, so the calculation lags the main loop index
+     * by (L-1)/2. The sample at index K is the median value of samples [K-(L-1)/2 ... K+(L-1)/2].
+     * Therefore after receiving the sample at K, the main loop updates the median filtered value
+     * at index M = K-(L-1)/2, since only at that point all the neede "future" samples are available.
+     * Note also that since L is assumed odd, we rely on the automatic rounding down of integer
+     * division, so we can use L/2 and not (L-1)/2.
+     */
     private float MedFiltSign() {
-        int m = k - (MEDFILT_LENGTH / 2);
-        for (int i = 0; i < MEDFILT_LENGTH; i++, m++) {
-            medFiltArray[i] = signalDataSign[m % WINDOW_SIZE];
+        for (int i = m - (MEDFILT_LENGTH / 2), j = 0; j < MEDFILT_LENGTH; i++, j++) {
+            medFiltArray[j] = signalDataSign[(i + WINDOW_SIZE) % WINDOW_SIZE];
         }
         Arrays.sort(medFiltArray);
         return medFiltArray[MEDFILT_LENGTH / 2];
